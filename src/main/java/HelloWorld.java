9207361b906c4871df7b15f33e8d96786e112c0f
unknown <juan.monroy-s@labinfo.is.escuelaing.edu.co>
2022-10-07 12:02:46 -0500

    Llaves

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        port(5000);
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
